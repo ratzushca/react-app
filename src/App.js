2478552c9ed5439aba4f7b83660a23ebd39dec45
ratzushca <93014061+ratzushca@users.noreply.github.com>
2022-03-09 19:55:14 -0800

    add row working

--- src/App.js
+++ src/App.js
@@ -2,54 +2,72 @@ import React,{useState} from "react"
 import ReactDOM from "react-dom"
 import Head from "./Head"
 import Nav from "./Nav"
+import {nanoid} from "nanoid"
 import InputBox from "./InputBox"
 import AddButton from "./AddButton"
 import Table from "./Table"
 
 
 export default function App(){
-    const [addFormData, setAddFormData] = useState([{
+
+    const [tableData, setTableData] = useState([])
+    const [addFormData, setAddFormData] = useState({
         type: "",
         date: "",
         name: "",
         amount: "",
-    }]);
+    }
+    );
 
       const handleAddFormChange = (event) => {
         event.preventDefault();
-    
-        const fieldName = event.target.getAttribute("name");
-        const fieldValue = event.target.value;
-    
-        const newFormData = { ...addFormData };
-        newFormData[fieldName] = fieldValue;
-    
-        setAddFormData(newFormData);
-      };
+  
+        const newInput = (data)=>({...data, [event.target.name]:event.target.value})
+        setAddFormData(newInput)
+ }
+     
 
       const handleAddFormSubmit = (event) => {
         event.preventDefault();
     
-        const newData = {
-          type: addFormData.type,
-          date: addFormData.date,
-          name: addFormData.name,
-          amount: addFormData.amount,
-        };
 
-        const newDatas = [...newData];
-        setAddFormData(newDatas);
-      };
+        const checkEmptyInput = !Object.values(addFormData).every(res=>res==="")
+        if(checkEmptyInput)
+        {
+         const newData = (data)=>([...data, addFormData])
+         setTableData(newData);
+         const emptyInput= {type:'', date:'', name:'', amount:''}
+         setAddFormData(emptyInput)
+        }
+    } 
+
+
+
+
+
+
+      //   const newData = {
+      //     id: nanoid(),
+      //     type: addFormData.type,
+      //     date: addFormData.date,
+      //     name: addFormData.name,
+      //     amount: addFormData.amount,
+      //   };
+
+      //   const newRow = [...addFormData,];
+      //   setAddFormData(newRow);
+      // };
     return(
         <div>
         <Nav />
         <Head />
-        <form onSubmit={handleAddFormSubmit}>
+        <React.StrictMode>
+        <form  onSubmit={handleAddFormSubmit}>
 
      <div className="form-inline">
         <div className="form-group col-md-4">
             <label className="type-label">Type:</label> 
-            <select className="form-control type-input" name="type" onChange={handleAddFormChange}>
+            <select className="form-control type-input" name="type" value={addFormData.type} onChange={handleAddFormChange}>
                 <option  >Cash</option>
                 <option >Card</option>
                 <option >Crypto</option>
@@ -61,30 +79,30 @@ export default function App(){
 
         <div className="form-group col-md-4">
             <label className="date-options date-label">Date:</label> 
-            <input type = "date" className="form-control date-input" name="date" onChange={handleAddFormChange}/>
+            <input type = "date" className="form-control date-input" name="date"value={addFormData.date} onChange={handleAddFormChange}/>
         </div>
     </div>
 
     <div className="form-inline">
         <div className="form-group col-md-4">
             <label className="name-label" >Name:</label> 
-            <input type="text" placeholder="What did you spend on?" className="form-control name-input" name="name" onChange={handleAddFormChange}/>
+            <input type="text" placeholder="What did you spend on?" className="form-control name-input" name="name" value={addFormData.name} onChange={handleAddFormChange}/>
             </div>
             
 
 
         <div className="form-group col-md-4">
             <label className="date-options amount-label">Amount:</label> 
-            <input type="number"  placeholder="How much?"className="form-control amount-input" name="amount" onChange={handleAddFormChange}/>
+            <input type="number"  placeholder="How much?"className="form-control amount-input" name="amount" value={addFormData.amount} onChange={handleAddFormChange}></input>
         </div>
     </div>
 
         </form>
         <div className ="col text-center">
-        <button  type="submit" className ="btn btn-primary" value="Submit" >Add a new expense</button>
+        <button  type="submit" className ="btn btn-primary" value="Submit"onClick={handleAddFormSubmit} >Add a new expense</button>
         </div>
         <table className="table table-striped table-hover">
-  <thead>
+  <thead key = "thead">
     <tr>
       <th scope="col" className=" bg-warning" >Type</th>
       <th scope="col"className=" bg-warning">Date</th>
@@ -92,18 +110,20 @@ export default function App(){
       <th scope="col"className=" bg-warning">Amount</th>
     </tr>
   </thead>
-  <tbody>
-    {addFormData.map((tData)=>(
-     <tr>
-     <td scope="row">{tData.type}</td>
-     <td>{tData.date}</td>
-     <td>{tData.name}</td>
-     <td>{tData.amount}</td>
+  <tbody tbody="tbody">
+    {
+    tableData.map((data,index)=>(
+     <tr key={index}>
+     <td scope="row">{data.type}</td>
+     <td >{data.date}</td>
+     <td >{data.name}</td>
+     <td >${data.amount}</td>
    </tr>
     ))}
    
     </tbody>
   </table>
+  </React.StrictMode>
         </div>
     )
 }
