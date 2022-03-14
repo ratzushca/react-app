0f4fb353e94c91ac06776e5c3321c1ae47e482e1
Lennart Boecken <lsboecken@gmail.com>
2022-03-14 14:00:50 -0400

    code review

--- src/App.js
+++ src/App.js
@@ -1,88 +1,71 @@
-import React,{useState, useEffect} from "react"
-import ReactDOM from "react-dom"
-import Head from "./Head"
-import Nav from "./Nav"
-import {nanoid} from "nanoid"
-import InputBox from "./InputBox"
-import AddButton from "./AddButton"
-import Table from "./Table"
+import { useState, useEffect } from 'react';
+import Head from './Head';
+import Nav from './Nav';
+import { nanoid } from 'nanoid';
+import InputBox from './InputBox';
+import Table from './Table';
 
+const isFormDataFilled = (formData) =>
+  !Object.values(formData).every((item) => item === '');
 
-export default function App(){
+export default function App() {
+  const [tableData, setTableData] = useState(
+    JSON.parse(localStorage.getItem('tableData')) || []
+  );
 
+  const [addFormData, setAddFormData] = useState({
+    type: '',
+    date: '',
+    name: '',
+    amount: '',
+  });
 
+  const handleAddFormChange = (event) => {
+    setAddFormData((prevData) => {
+      return { ...prevData, [event.target.name]: event.target.value };
+    });
+  };
 
-    const [tableData, setTableData] = useState(localStorage.getItem('tableData')
-    ? JSON.parse(localStorage.getItem('tableData')): [])
-
-   
-    const [addFormData, setAddFormData] = useState(
-      {
-      
-        type: "",
-        date: "",
-        name: "",
-        amount: "",
+  const handleAddFormSubmit = () => {
+    if (isFormDataFilled(addFormData)) {
+      let newEntry = addFormData;
+      newEntry.id = nanoid();
+      setTableData((prevData) => [...prevData, newEntry]);
+      setAddFormData({
+        type: '',
+        date: '',
+        name: '',
+        amount: '',
+      });
+    } else {
+      alert('Form not filled out fully');
     }
-    
-    );
+  };
 
-  
+  const handleDeleteFunction = (rowId) => {
+    const newRows = [...tableData];
+    const index = tableData.findIndex((row) => row.id === rowId);
+    newRows.splice(index, 1);
+    setTableData(newRows);
+  };
 
-      const handleAddFormChange = (event) => {
-        event.preventDefault();
-
-        const newInput = (data)=>({...data, [event.target.name]:event.target.value})
-        setAddFormData(newInput)
- }
-     
-
-      const handleAddFormSubmit = (event) => {
-        event.preventDefault();
-    
-        const checkEmptyInput = !Object.values(addFormData).every(res=>res==="")
-        if(checkEmptyInput)
-        {
-         const newData = (data)=>([...data, addFormData])
-         setTableData(newData);
-         const emptyInput= {id: nanoid(),type:'', date:'', name:'', amount:''}
-         setAddFormData(emptyInput)
-        }
-    } 
-
-      const hadleDeleteFunction=(rowId)=>{
-        const newRows = [...tableData];
-        const index = tableData.findIndex((row)=>row.id===rowId);
-        newRows.splice(index,1);
-        setTableData(newRows);
-      };
-
-
-     
   useEffect(() => {
-    localStorage.setItem('tableData', JSON.stringify(tableData))
-  }, [tableData])
+    localStorage.setItem('tableData', JSON.stringify(tableData));
+  }, [tableData]);
 
- 
-
-
-    return(
-        <div>
-        <Nav />
-        <Head />
-        <React.StrictMode>
-        <InputBox 
+  return (
+    <>
+      <Nav />
+      <Head />
+      <InputBox
         handleAddFormSubmit={handleAddFormSubmit}
-        handleAddFormChange={handleAddFormChange} 
-        addFormData={addFormData}/>
-        <AddButton
-        handleAddFormSubmit={handleAddFormSubmit}
-   
-        />
-        <Table
-        tableData={tableData} 
-        hadleDeleteFunction={hadleDeleteFunction}/>
-        </React.StrictMode>
-        </div>
-    )
+        handleAddFormChange={handleAddFormChange}
+        addFormData={addFormData}
+      />
+      <Table
+        tableData={tableData}
+        handleDeleteFunction={handleDeleteFunction}
+      />
+    </>
+  );
 }
--- src/InputBox.js
+++ src/InputBox.js
@@ -1,43 +1,72 @@
-import React from "react"
+export default function InputBox({
+  handleAddFormChange,
+  handleAddFormSubmit,
+  addFormData,
+}) {
+  return (
+    <div className='row inputboxes'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddFormSubmit();
+        }}>
+        <div className='form-inline row'>
+          <div className='form-group col-md-4 col-lg-4'>
+            <label className='type-label'>Type:</label>
+            <select
+              className='form-control type-input'
+              name='type'
+              value={addFormData.type}
+              onChange={handleAddFormChange}>
+              <option>Cash</option>
+              <option>Card</option>
+              <option>Crypto</option>
+              <option>Other</option>
+            </select>
+          </div>
 
-export default function InputBox({handleAddFormChange,handleAddFormSubmit,addFormData}){
-    return(
-        <div className="row inputboxes">
-        <form   onSubmit={handleAddFormSubmit}>
+          <div className='form-group col-md-4 col-lg-4'>
+            <label className='date-options date-label'>Date:</label>
+            <input
+              type='date'
+              className='form-control date-input'
+              name='date'
+              value={addFormData.date}
+              onChange={handleAddFormChange}
+            />
+          </div>
+        </div>
 
-        <div className="form-inline row">
-           <div className="form-group col-md-4 col-lg-4">
-               <label className="type-label">Type:</label> 
-               <select className="form-control type-input" name="type" value={addFormData.type} onChange={handleAddFormChange}>
-                   <option  >Cash</option>
-                   <option >Card</option>
-                   <option >Crypto</option>
-                   <option >Other</option>
-                   </select>
-               </div>
-   
-           <div className="form-group col-md-4 col-lg-4">
-               <label className="date-options date-label">Date:</label> 
-               <input type = "date" className="form-control date-input" name="date"value={addFormData.date} onChange={handleAddFormChange}/>
-           </div>
-       </div>
+        <div className='form-inline row'>
+          <div className='form-group col-md-4 col-lg-4'>
+            <label className='name-label'>Name:</label>
+            <input
+              type='text'
+              placeholder='What did you spend on?'
+              className='form-control name-input'
+              name='name'
+              value={addFormData.name}
+              onChange={handleAddFormChange}
+            />
+          </div>
 
-   
-       <div className="form-inline row">
-           <div className="form-group col-md-4 col-lg-4">
-               <label className="name-label" >Name:</label> 
-               <input type="text" placeholder="What did you spend on?" className="form-control name-input" name="name" value={addFormData.name} onChange={handleAddFormChange}/>
-               </div>
-               
-   
-   
-           <div className="form-group col-md-4 col-lg-4">
-               <label className="date-options amount-label">Amount:</label> 
-               <input type="number"  placeholder="How much?"className="form-control amount-input" name="amount" value={addFormData.amount} onChange={handleAddFormChange}></input>
-           </div>
-       </div>
-   
-           </form>
-           </div>
-    )
+          <div className='form-group col-md-4 col-lg-4'>
+            <label className='date-options amount-label'>Amount:</label>
+            <input
+              type='number'
+              placeholder='How much?'
+              className='form-control amount-input'
+              name='amount'
+              value={addFormData.amount}
+              onChange={handleAddFormChange}></input>
+          </div>
+        </div>
+        <div className='col text-center add-btn'>
+          <button type='submit' className='btn btn-primary' value='Submit'>
+            Add a new expense
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
